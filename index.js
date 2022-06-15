e24ff1741cc3ea39fe0ff3b6c478b41dc857e5a3
harsh1210kaswan <harsh_2k19ec069@dtu.ac.in>
2022-06-15 14:32:43 +0530

    Check

---
+++ index.js
@@ -0,0 +1,8 @@
+a="hello";
+console.log(a);
+var n=1000;
+console.log(`Printing one more than num here num is ${n} and after incrementing value is ${n+1}`)
+let l=100;
+console.log(l);
+l="ABCD"
+console.log(l);
